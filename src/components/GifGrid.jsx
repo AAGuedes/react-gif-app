656ca4de56671e8cbb2d42ac3ff733f8e8927140
Airam A. <airam.a.g.85@gmail.com>
2023-11-12 19:30:46 +0000

    useEffect

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,9 +1,12 @@
+import { useState, useEffect } from 'react';
 import { getGifs } from '../helpers/getGifs';
 
 
 export const GifGrid = ({ category }) => {
 
-    getGifs(category);
+    useEffect(() => {
+        getGifs(category);
+    }, []);
 
     return (
         <>
