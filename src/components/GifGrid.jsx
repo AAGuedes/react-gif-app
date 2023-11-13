9ce0369bd54d55c40b4746fed0fcd09f805d277e
Airam A. <airam.a.g.85@gmail.com>
2023-11-13 21:34:53 +0000

    Mostrar mensaje de carga

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -9,9 +9,15 @@ export const GifGrid = ({ category }) => {
     return (
         <>
             <h3 className='text-center mt-5 mb-3'>{category}</h3>
+
             <div className='d-flex row d-flex justify-content-evenly'>
                 {
-                    images.map((image) => (
+                    isLoading && (
+                        <div className="d-flex justify-center spinner-border m-5" role="status" />
+                    )
+                }
+                {
+                    !isLoading && images.map((image) => (
                         <GifItem
                             key={image.id}
                             {...image}
