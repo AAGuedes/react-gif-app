939d4b94dd52d513007439ecdab4561b125317e1
Airam A. <airam.a.g.85@gmail.com>
2023-11-16 14:43:44 +0000

    Pruebas del componente - GifGridItem

--- tests/components/GifItem.test.jsx
+++ tests/components/GifItem.test.jsx
@@ -1,17 +1,28 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { GifItem } from "../../src/components/GifItem";
 
 
 describe('Pruebas de <GifItem />', () => {
 
     const title = 'Title';
-    const url = 'https://www.localhost.es/';
+    const url = 'https://www.localhost.com/';
 
     test('Debe coincidir con la snapshot', () => {
-
         const { container } = render(<GifItem title={title} url={url} />);
         expect(container).toMatchSnapshot();
+    });
 
-    })
+    test('Debe mostrar la imágen con el URL y ALT indicados', () => {
+        render(<GifItem title={title} url={url} />);
+        // screen.debug();
+        const { src, alt } = screen.getByRole('img');
+        expect(src).toBe(url);
+        expect(alt).toBe(title);
+    });
+
+    test('Debe mostrar el título en el componente', () => {
+        render(<GifItem title={title} url={url} />);
+        expect(screen.getByText(title)).toBeTruthy();
+    });
 
 });
