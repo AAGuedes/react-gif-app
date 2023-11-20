a1b3b0b2af1e2c166c36688a4627d69c7dfe90bd
Airam A. <airam.a.g.85@gmail.com>
2023-11-20 14:09:25 +0000

    Hacer un mock completo de un Custom Hook

--- tests/components/GifGrid.test.jsx
+++ tests/components/GifGrid.test.jsx
@@ -1,11 +1,19 @@
 import { render, screen } from "@testing-library/react";
 import { GifGrid } from "../../src/components/GifGrid";
+import { useFetchGifs } from "../../src/hooks/useFetchGifs";
+
+jest.mock('../../src/hooks/useFetchGifs');
 
 describe('Pruebas de <GifGrid />', () => {
 
     const category = 'John Wick';
 
     test('Debe mostrar el loading inicialmente', () => {
+
+        useFetchGifs.mockReturnValue({
+            images: [],
+            isLoading: true
+        });
 
         render(<GifGrid category={category} />);
 
@@ -15,7 +23,27 @@ describe('Pruebas de <GifGrid />', () => {
 
     test('Debe mostrar items cuando se carguen imágenes desde useFetchGifs', () => {
 
+        const gifs = [
+            {
+                id: 'ABC',
+                title: 'John Wick',
+                url: 'https://www.localhost.com/john_wick.png'
+            },
+            {
+                id: 'DEF',
+                title: 'John Wick II',
+                url: 'https://www.localhost.com/john_wick_2.png'
+            },
+        ];
 
+        useFetchGifs.mockReturnValue({
+            images: gifs,
+            isLoading: false
+        });
+
+        render(<GifGrid category={category} />);
+
+        expect(screen.getAllByRole('img').length).toBe(2);
     });
 
 });
